398bf280a4f5333afc790d59f71e7d1825e63eef
Charlie Vanaret <vanaret@zib.de>
2023-08-11 12:48:37 +0200

    Improved code and added check for empty directory

--- src/TimeSlice.py
+++ src/TimeSlice.py
@@ -9,25 +9,29 @@ import imageio
 
 # load image files
 def load_image_files(directory, extension):
+   # scan the directory for image files
    files = sorted(glob.glob(directory + '/*' + extension))
-   images = [imageio.imread(file) for file in files]
-   return images
+   if len(files) == 0:
+      raise Exception("Error: no image could be found.")
+   # load the images as RGB matrices
+   return [imageio.imread(file) for file in files]
 
 # check dimensions
 def check_image_dimensions(images):
    for i in range(len(images)-1):
-      if images[i].shape[0] != images[i+1].shape[0] or images[i].shape[1] != images[i+1].shape[1]:
+      if images[i].shape[0:2] != images[i+1].shape[0:2]:
          raise Exception("Error: the images do not have the same dimensions.")
 
 # perform chosen timeslice with given options
 def perform_timeslice(images):
+   (height, width) = images[0].shape[0:2]
    # TODO
    return images[0]
 
 # save result
 def save_result(result_image, result_file_name):
    imageio.imwrite(result_file_name, result_image[:, :, 0])
-   print("The timeslice was saved in the file %s " % result_file_name)
+   print("The timeslice was saved in the file %s." % result_file_name)
 
 # main function
 def main():
